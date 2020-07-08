cbd203ecf9109998ff4790e5e50923164eab1438
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-08 00:43:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,2 @@
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-introduction_with_language("Josh", "Python")
+  puts 
